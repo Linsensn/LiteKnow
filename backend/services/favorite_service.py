022251110fd2a94cdb900fb2e821141079fc789f
w <2377391579@qq.com>
2026-07-09 19:55:12 +0800

    feat:增加题库收藏

--- backend/services/favorite_service.py
+++ backend/services/favorite_service.py
@@ -9,7 +9,9 @@ from crud.messages_crud import msg_crud
 from schemas.favorite_schema import FavoriteCreate
 from schemas.common import PageResult
 from models.sessions import Session as SessionModel
+from models.question_banks import QuestionBank as QuestionBankModel
 from utils.exceptions import CustomAPIException, ErrorCode
+from loguru import logger
 
 
 def _sa_to_dict(obj):
@@ -33,6 +35,8 @@ class FavoriteService:
             if not folder:
                 folder = await favorite_crud.create(db, user_id=user_id, obj_in=obj_in.model_dump())
 
+            logger.debug("add_favorite | folder_id={} content_ids={} type={} content_id={}",
+                         folder.id, folder.content_ids, type(folder.content_ids), content_id)
             if content_id in (folder.content_ids or []):
                 db.commit()
                 return {"action": "exists", "message": "已收藏，无需重复添加"}
@@ -60,6 +64,8 @@ class FavoriteService:
                 db.commit()
                 return {"action": "removed", "message": "已取消收藏"}
 
+            logger.debug("remove_favorite | folder_id={} content_ids={} type={} content_id={}",
+                         folder.id, folder.content_ids, type(folder.content_ids), content_id)
             await favorite_crud.remove_content(db, db_obj=folder, content_id=content_id)
             db.commit()
             return {"action": "removed", "message": "已取消收藏"}
@@ -150,6 +156,8 @@ class FavoriteService:
         folder = await favorite_crud.get_by_type(db, user_id=user_id, content_type=content_type)
         if not folder:
             return {"is_favorited": False, "folder_id": None}
+        logger.debug("check_favorite_status | folder_id={} content_ids={} type={}",
+                     folder.id, folder.content_ids, type(folder.content_ids))
         return {
             "is_favorited": content_id in (folder.content_ids or []),
             "folder_id": folder.id
@@ -163,6 +171,8 @@ class FavoriteService:
         folder = await favorite_crud.get_by_type(db, user_id=user_id, content_type=content_type)
         if not folder:
             return {str(cid): False for cid in content_ids}
+        logger.debug("batch_check_status | folder_id={} content_ids={} type={}",
+                     folder.id, folder.content_ids, type(folder.content_ids))
         fav_set = set(folder.content_ids or [])
         return {str(cid): cid in fav_set for cid in content_ids}
 
@@ -182,6 +192,9 @@ class FavoriteService:
         end = start + page_size
         page_ids = content_ids[start:end]
 
+        logger.debug("get_folder_contents | folder_id={} content_ids={} type={} page_ids={}",
+                     folder.id, content_ids, type(content_ids), page_ids)
+
         # 按 content_type 分发到不同业务表
         if not page_ids:
             items = []
@@ -193,9 +206,17 @@ class FavoriteService:
             items = [_sa_to_dict(o) for o in db_items]
         elif folder.content_type == "session":
             stmt = select(SessionModel).where(SessionModel.id.in_(page_ids))
-            result = await db.execute(stmt)
+            result = db.execute(stmt)
             db_items = result.scalars().all()
             items = [_sa_to_dict(o) for o in db_items]
+
+        # ✨ 新增 bank (题库) 的查询分支
+        elif folder.content_type == "bank":
+            stmt = select(QuestionBankModel).where(QuestionBankModel.id.in_(page_ids))
+            result = db.execute(stmt)
+            db_items = result.scalars().all()
+            items = [_sa_to_dict(o) for o in db_items]
+
         else:
             # knowledge 或其他类型暂不支持
             items = []
